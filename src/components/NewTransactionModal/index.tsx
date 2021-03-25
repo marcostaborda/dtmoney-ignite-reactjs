8128620edbe1315491d8e39dfda19896c6823cd1
Marcos Taborda <marcos.tabordamail@gmail.com>
2021-03-25 18:17:32 -0300

    feat: add modal global

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,5 @@
 import Modal from 'react-modal';
+import { Container } from './styles';
 
 interface NewTransactionModalProps {
   onCloseRequestModal : () => void;
@@ -9,8 +10,21 @@ Modal.setAppElement("#root");
 
 export function NewTransactionModal({ isOpenModal, onCloseRequestModal  }: NewTransactionModalProps) {
   return (
-    <Modal isOpen={isOpenModal} onRequestClose={onCloseRequestModal}>
-      
+    <Modal 
+      isOpen={isOpenModal} 
+      onRequestClose={onCloseRequestModal}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
+    >
+      <Container> 
+        <h2>Cadastrar Transação</h2>
+        <input type="text" placeholder="Título"/>
+        <input type="text" placeholder="Valor"/>
+        <input type="text" placeholder="Categoria"/>
+        <button type="submit">
+          Cadastrar
+        </button>
+      </Container>
     </Modal>
   )
 }
