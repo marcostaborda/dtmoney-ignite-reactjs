960baf23db7158c668071cfec06a0b9676b248b9
Marcos Taborda <marcos.tabordamail@gmail.com>
2021-03-25 18:45:54 -0300

    feat: criação dos bottons de tipos transações.

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,30 +1,44 @@
 import Modal from 'react-modal';
-import { Container } from './styles';
+import { Container, ButtonCloseModal, TransactionTypeContainer } from './styles';
 import closeImg from '../../assets/close.svg';
+import incomeImg from '../../assets/income.svg';
+import outcomeImg from '../../assets/outcome.svg';
 
 interface NewTransactionModalProps {
-  onCloseRequestModal : () => void;
+  onCloseRequestModal: () => void;
   isOpenModal: boolean;
 }
 
 Modal.setAppElement("#root");
 
-export function NewTransactionModal({ isOpenModal, onCloseRequestModal  }: NewTransactionModalProps) {
+export function NewTransactionModal({ isOpenModal, onCloseRequestModal }: NewTransactionModalProps) {
   return (
-    <Modal 
-      isOpen={isOpenModal} 
+    <Modal
+      isOpen={isOpenModal}
       onRequestClose={onCloseRequestModal}
       overlayClassName="react-modal-overlay"
       className="react-modal-content"
     >
-      <button type="button" onClick={onCloseRequestModal}>
-        <img src={closeImg} alt="Sair do modal"/>
-      </button>
-      <Container> 
+      <ButtonCloseModal type="button" onClick={onCloseRequestModal} className="button-close-react-modal">
+        <img src={closeImg} alt="Sair do modal" />
+      </ButtonCloseModal>
+      <Container>
         <h2>Cadastrar Transação</h2>
-        <input type="text" placeholder="Título"/>
-        <input type="text" placeholder="Valor"/>
-        <input type="text" placeholder="Categoria"/>
+        <input type="text" placeholder="Título" />
+        <input type="text" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button">
+            <img src={incomeImg} alt="Entrada" />
+            <span>Entrada</span>
+          </button>
+          <button type="button">
+            <img src={outcomeImg} alt="Saída" />
+            <span>Saída</span>
+          </button>
+        </TransactionTypeContainer>
+
+        <input type="text" placeholder="Categoria" />
         <button type="submit">
           Cadastrar
         </button>
--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -1,10 +1,11 @@
 import styled from 'styled-components';
+import { darken } from 'polished';
 
 export const Container = styled.form`
-  h1 {
+  h2 {
     color: var(--text-title);
     font-size: 1.5rem;
-    margin-bottom: 2rem;
+    margin-bottom: 1rem;
   }
 
   input {
@@ -45,19 +46,56 @@ export const Container = styled.form`
       filter: brightness(0.9);
     }
   }
+`;
 
-  button[type="button"] {
-    position: absolute;
-    right: 1.5rem;
-    top: 1.5rem;
-    border: 0;
+export const ButtonCloseModal = styled.button`
+  position: absolute;
+  right: 1.8rem;
+  top: 1.5rem;
+  border: 0;
+  background: transparent;
+
+  transition: filter 0.2s;
+
+  &:hover{
+    filter: brightness(0.7);
+    transform: translate(0, 50);
+  } 
+`;
+
+export const TransactionTypeContainer = styled.div`
+  margin: 1rem 0;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  gap: 0.5rem;
+
+
+  button {
+    height: 4rem;
+    border: 1px solid #d7d7d7;
+
     background: transparent;
 
-    transition: filter 0.2s;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-    &:hover{
-      filter: brightness(0.7);
-      transform: translate(0, 50);
+    transition: border-color 0.2s;
+
+    &:hover {
+      border-color: ${darken(0.1, '#d7d7d7')}
+    }
+
+    img {
+      width: 20px;
+      height: 20px;
+    }
+
+    span {
+      display: inline-block;
+      margin-left: 1rem;
+      font-size: 1rem;
+      color: var(--text-title);
     }
   }
 `;
